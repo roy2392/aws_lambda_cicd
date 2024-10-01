22759daea0ce1acafd2f87e859654eac122efb3d
Roey Zalta <roey.zalta@gmail.com>
2024-10-01 15:09:03 +0300

    test new workflow changes

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda - this is Roey!')
+        'body': json.dumps('Hello from Lambda - this is Roey Zalta!')
     }
