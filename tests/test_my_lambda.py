70327ab687fe3471f1c8124a45de25c13431c997
Roey Zalta <roey.zalta@gmail.com>
2024-10-01 15:05:32 +0300

    change workflow to run test before deploy

--- tests/test_my_lambda.py
+++ tests/test_my_lambda.py
@@ -1,6 +1,18 @@
 from src.lambda_function import lambda_handler
+import json
 
-def test_my_function():
-    result = lambda_handler(5)
-    expected = 25
-    assert result == expected, f"Expected {expected}, got {result}"
+
+def test_lambda_handler():
+    # Arrange
+    event = {}
+    context = {}
+    expected_status_code = 200
+    expected_body = 'Hello from Lambda - this is Roey!'
+
+    # Act
+    response = lambda_handler(event, context)
+
+    # Assert
+    assert response['statusCode'] == expected_status_code
+    assert json.loads(response['body']) == expected_body
+
